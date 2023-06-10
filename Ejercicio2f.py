79771c72c4c8252b506fd2351bf1e584689be404
Reyser Julio Zapata Butron <rzapata@unsa.edu.pe>
2023-06-09 22:05:03 -0500

    Ejercicio2f terminado

--- Ejercicio2f.py
+++ Ejercicio2f.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+columna1 = square.join(square.negative()).horizontalRepeat(4)
+columna2 = square.negative().join(square).horizontalRepeat(4)
+
+tablero = columna1.under(columna2).verticalRepeat(2)
+
+draw(tablero)
