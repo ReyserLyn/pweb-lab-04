c4ccf927e05260170d8798b785de39d4c4404de0
Reyser Julio Zapata Butron <rzapata@unsa.edu.pe>
2023-06-09 22:34:19 -0500

    Ejercicio2g terminado

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -1,2 +1,18 @@
 from interpreter import draw
 from chessPictures import *
+
+columna1 = square.join(square.negative()).horizontalRepeat(4)
+columna2 = square.negative().join(square).horizontalRepeat(4)
+
+tablero = columna1.under(columna2).verticalRepeat(4)
+
+peones = pawn.horizontalRepeat(8)
+negras = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock).under(peones).negative()
+espacios = columna1.under(columna2).verticalRepeat(2)
+blancas = peones.under(rock.join(knight).join(bishop).join(king).join(queen).join(bishop).join(knight).join(rock))
+
+piezas = negras.under(espacios).under(blancas)
+
+
+
+draw(piezas.up(tablero))
