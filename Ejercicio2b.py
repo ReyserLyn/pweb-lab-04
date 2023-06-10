6348c37faa9cf13834f0c3049f6c5c9d03ade695
Reyser Julio Zapata Butron <rzapata@unsa.edu.pe>
2023-06-09 21:55:17 -0500

    Ejercicio2b terminado

--- Ejercicio2b.py
+++ Ejercicio2b.py
@@ -1,2 +1,11 @@
 from interpreter import draw
 from chessPictures import *
+
+caballoBlanco = knight
+caballoNegro = knight.negative()
+
+columna1 = caballoBlanco.join(caballoNegro)
+columna2 = caballoNegro.horizontalMirror().join(caballoBlanco.horizontalMirror())
+
+tablero = columna1.under(columna2)
+draw(tablero)
